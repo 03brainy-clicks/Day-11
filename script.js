135ecffa7a82f4a4738f8039276f5ce8a73eff0a
Shivam <chandrakant.kushwah2019@sait.ac.in>
2022-02-19 04:43:50 +0530

    added first joke

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // global
-let jokes = document.getElementById('joke');
+let jokes = document.getElementById("joke");
 // global
-
 
 // function for getting data
 async function joke() {
-    let joke = await fetch("https://api.chucknorris.io/jokes/random");
-    let data = await joke.json();
-    jokes.innerText = data.value;
+  let joke = await fetch("https://api.chucknorris.io/jokes/random");
+  let data = await joke.json();
+  jokes.innerText = data.value;
 }
+joke();
 // function for getting data
