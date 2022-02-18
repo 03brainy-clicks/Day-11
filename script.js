27dcfeff3aac1e1a984da3814cc1dcca345f13a6
Shivam <chandrakant.kushwah2019@sait.ac.in>
2022-02-18 20:14:18 +0530

    added files for joke generator

---
+++ script.js
@@ -0,0 +1,12 @@
+// global
+let jokes = document.getElementById('joke');
+// global
+
+
+// function for getting data
+async function joke() {
+    let joke = await fetch("https://api.chucknorris.io/jokes/random");
+    let data = await joke.json();
+    jokes.innerText = data.value;
+}
+// function for getting data
